5a31e106d7659294c413cd8b8256f3692777670d
Irae Bornholdt <iraeborn@gmail.com>
2021-10-18 21:09:34 -0400

    model and migration

---
+++ app/Models/Devs.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Devs extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'sexo',
+        'idade',
+        'hobby',
+        'datanascimento',
+    ]
+}
